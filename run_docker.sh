48befc991ff91d87a8a8f2a1b469c5693ee8e3ca
Davide Bove <me@davidebove.com>
2016-07-05 13:29:28 +0200

    Add check for existing instance of docker image

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,19 @@
 # --add-host=db:172.17.0.2 
 #get postgres ip
 id=$(docker ps -q -f ancestor=postgres)
+id2=$(docker ps -q -f ancestor=django_web)
 if [ -z "$id" ] 
 then
         echo "[!] Error: Start the postgres image before running django! Try: docker run -d postgres"
         exit 1
 fi
 
-docker run --link $id:db -v "$(pwd)"/src:/code -p 8000:8000 -t -i django_web python /code/manage.py runserver 0.0.0.0:8000
+if [ -z "$id2" ]
+then
+    clear
+    docker run --link $id:db -v "$(pwd)"/src:/code -p 8000:8000 -t -i django_web python /code/manage.py runserver 0.0.0.0:8000
+else
+    clear
+    echo "[!] Warning: Docker instance already running! Connecting to existing one.."
+    docker exec -it $id2 /bin/bash
+fi
